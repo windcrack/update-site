d67a730518f830fe6a0423e3257981ee6af42d43
Alexander Voevodin <43845313+windcrack@users.noreply.github.com>
2022-07-05 12:22:24 +0300

    hot hux img in modal

--- src/components/NewmemberNav/index.js
+++ src/components/NewmemberNav/index.js
@@ -6,47 +6,47 @@ export default function NewmemberNav(){
             <h2 className="title__sub">Коротко о нас</h2>
             <ul className="newMemberNav-items">
                 <li className="newMemberNav-item">
-                    <Link className="newMemberNav-link white" to="">
+                    <Link className="newMemberNav-link white" to="/twelvestep">
                         Анонимные Наркоманы — это бесплатное анонимное сообщество, которое поможет тебе отказаться от наркотиков и научиться жить без них.
                     </Link>
                 </li>
                 <li className="newMemberNav-item">
-                    <Link className="newMemberNav-link white" to="">
+                    <Link className="newMemberNav-link white" to="/timetable">
                         Собрания проходят по всему миру круглосуточно. Не нужно никаких документов, можно назваться своим или вымышленным именем, прийти и уйти в любой момент.
                     </Link>
                 </li>
                 <li className="newMemberNav-item">
-                    <Link className="newMemberNav-link white" to="">
+                    <Link className="newMemberNav-link white" to="/howcan">
                         Все, что нужно, чтобы стать членом Анонимных Наркоманов — желание бросить употреблять наркотики.
                     </Link>
                 </li>
                 <li className="newMemberNav-item">
-                    <Link className="newMemberNav-link white" to="">
+                    <Link className="newMemberNav-link white" to="/an-meeting">
                         Тебя никто не осудит, если на первые собрания ты придёшь, ещё употребляя наркотики. Многие из нас начинали именно так.
                     </Link>
                 </li>
                 <li className="newMemberNav-item">
-                    <Link className="newMemberNav-link white" to="">
+                    <Link className="newMemberNav-link white" to="/timetable">
                         Не важно, какие именно наркотики ты предпочитаешь. Анонимные Наркоманы помогают отказаться от любых веществ, изменяющих сознание.
                     </Link>
                 </li>
                 <li className="newMemberNav-item">
-                    <Link className="newMemberNav-link white" to="">
+                    <Link className="newMemberNav-link white" to="/howcan">
                         Анонимные Наркоманы не имеют отношения к религии, политике, медицине.
                     </Link>
                 </li>
                 <li className="newMemberNav-item">
-                    <Link className="newMemberNav-link white" to="">
+                    <Link className="newMemberNav-link white" to="/members">
                         В нашем сообществе нет никаких взносов или других платежей «за участие».
                     </Link>
                 </li>
                 <li className="newMemberNav-item">
-                    <Link className="newMemberNav-link white" to="">
+                    <Link className="newMemberNav-link white" to="/an-meeting">
                         Для нас очень важно сохранять анонимность, поэтому мы просим не разглашать услышанную на собраниях информацию.
                     </Link>
                 </li>
                 <li className="newMemberNav-item">
-                    <Link className="newMemberNav-link white" to="">
+                    <Link className="newMemberNav-link white" to="/twelvestep">
                         Группы Анонимных Наркоманов и программа 12 Шагов помогли многим зависимым. Возможно, помогут и тебе. Попробуй!
                     </Link>
                 </li>
